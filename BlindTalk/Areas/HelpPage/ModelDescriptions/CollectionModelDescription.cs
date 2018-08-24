2e49bec6c0217ca6da0d385a35fe4f937693889b
DESKTOP-PIM0KDV\Bryniolf <ireneusz.mielcarek94@gmail.com>
2018-08-24 11:48:34 +0200

    Stworzenie nowego projektu

---
+++ BlindTalk/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace BlindTalk.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ BlindTalk/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BlindTalk.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
